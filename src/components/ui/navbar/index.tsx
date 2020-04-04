db415ae2778ef83655174db9815c0fd705b52fb0
davidvargash <dvargas@zenfolio.com>
2020-04-04 16:37:43 -0500

    back button added

--- src/components/ui/navbar/index.tsx
+++ src/components/ui/navbar/index.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import styles from "./navbar.module.scss";
 import logo from "icons/logo.svg";
+import back from "icons/back.svg";
 import Search from "../search";
 import { Link } from "react-router-dom";
 import { withRouter, RouteComponentProps } from "react-router";
 
 const Navbar: React.FC<RouteComponentProps> = (props) => {
-  console.log(props.location.pathname);
   return (
     <nav className={styles.navbar}>
       <div className={styles.content}>
-        <Link to="/home">
-          <img className={styles.logo} alt="logo" src={logo} />
-        </Link>
-        {props.location.pathname.includes("animals") && (
-          <Search small query={""} />
+        <div className={styles.left}>
+          <Link to="/home">
+            <img className={styles.logo} alt="logo" src={logo} />
+          </Link>
+          {props.location.pathname.includes("animals") && (
+            <Search small query={""} />
+          )}
+        </div>
+        {!props.location.pathname.includes("home") && (
+          <img
+            className={styles.back}
+            src={back}
+            alt="back"
+            title="go back"
+            onClick={(e) => props.history.goBack()}
+          />
         )}
       </div>
     </nav>
